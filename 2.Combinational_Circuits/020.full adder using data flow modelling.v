6d9965936cdce1d1da0139b7fe42d1ce09e7a8bc
mano-3-1-2002 <manovijay383@gmail.com>
2024-10-09 16:18:08 +0530

    Update 020.full adder using data flow modelling.v

--- 2.Combinational_Circuits/020.full adder using data flow modelling.v
+++ 2.Combinational_Circuits/020.full adder using data flow modelling.v
@@ -1,9 +1,5 @@
 module full_ad(input a,b,cin,output sum,carry);
-  wire x,y,z;
-  xor (x,a,b);
-  xor(sum,x,cin);
-  and(y,a,b);
-  and(z,x,cin);
-  or(carry,y,z);
+  assign sum = a^b^cin;
+  assign carry = (a&b)|(b&cin)|(cin&a);
 endmodule
     
