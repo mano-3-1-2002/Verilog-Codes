93ba638a07d1a87b0b6f67ae8974e49d3806af5e
mano-3-1-2002 <manovijay383@gmail.com>
2024-11-11 16:40:42 +0530

    Create 064.3:8decoder.v

---
+++ 4.Combinational_Circuits_3/064.3:8decoder.v
@@ -0,0 +1,15 @@
+module decoder_3_1(input[2:0]d,output reg[7:0]y);
+  always@(d)begin
+    casex(d)
+      3'b000:y=8'b00000001;
+      3'b001:y=8'b00000010;
+      3'b010:y=8'b00000100;
+      3'b011:y=8'b00001000;
+      3'b100:y=8'b00010000;
+      3'b101:y=8'b00100000;
+      3'b110:y=8'b01000000;
+      3'b111:y=8'b10000000;
+      default:$display("invalid");
+    endcase
+  end
+endmodule
