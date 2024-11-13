8960d2b1db3b8d409b31a180c0e5ac49b4b050ad
mano-3-1-2002 <manovijay383@gmail.com>
2024-11-13 18:47:00 +0530

    Create sr_latch_using nand gate.v

---
+++ 5.sequential_circuits/sr_latch_using nand gate.v
@@ -0,0 +1,16 @@
+module sr_latch(input s,r,output reg q,qbar);
+  always @(s or r)begin
+    if(~s && r)begin
+      q=1;
+      qbar=0;
+    end
+    else if(s&&~r)begin
+      q=0;
+      qbar=1;
+    end
+    else if(~s&&~r)begin
+      q=1;
+      qbar=1;
+    end
+  end
+endmodule
