751b42199b87e5a97e7977c2e3856acf98ac3931
mano-3-1-2002 <manovijay383@gmail.com>
2024-11-18 20:05:58 +0530

    Create 098.Melay 1010 non-overlapping testbench.v

---
+++ 6.Finite State Machines(FSM)/098.Melay 1010 non-overlapping testbench.v
@@ -0,0 +1,34 @@
+module tb;
+  reg i,clk,rst;
+  wire y;
+  fsm_1010 f1(.i(i),.clk(clk),.rst(rst),.y(y));
+  always #2 clk = ~clk;
+  initial begin
+    $monitor("%t:clk=%0b,rst=%0b,i=%0b,y=%0b",$time,clk,rst,i,y);
+    clk=0;
+    i=0;#1;
+    rst=0;#2;
+ 
+    rst=1;#3;
+    i=1;#4;
+    i=1;#4;
+    i=0;#4;
+    i=1;#4;
+    i=0;#4;
+    i=1;#4;
+    i=0;#4;
+    i=1;#4;
+    i=1;#4;
+    i=1;#4;
+    i=0;#4;
+    i=1;#4;
+    i=0;#4;
+    i=1;#4;
+    i=0;#10;
+    $finish;
+  end
+  initial begin
+    $dumpfile("fsm_1010.vcd");
+    $dumpvars;
+  end
+endmodule
