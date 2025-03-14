bfe22df80d352aca6c74b1693f5105a0361441c5
Manobala.S <manovijay383@gmail.com>
2025-03-14 22:11:17 +0530

    Create 124.Serial In Serial Out register_tb.v

---
+++ 7.Counters&Registers/124.Serial In Serial Out register_tb.v
@@ -0,0 +1,25 @@
+module siso_tb;
+  reg clk;
+  reg rst;
+  reg si;
+  wire so;
+  siso sr(.clk(clk),.rst(rst),.si(si),.so(so));
+  always #2 clk = ~clk;
+  initial begin
+    $monitor("time=%t,so=%0b",$time,so);
+    clk = 0;
+    rst = 1;
+    si = 0;
+    #6 rst = 0;
+    si = 1;#6
+    si = 0;#6
+    si = 1;#6
+    si = 1;
+    #10
+    $finish;
+  end
+  initial begin
+    $dumpfile("siso.vcd");
+    $dumpvars;
+  end
+endmodule
