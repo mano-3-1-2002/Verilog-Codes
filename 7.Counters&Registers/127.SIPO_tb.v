ca4ead4615a0fe515a5c70a1e9c4e9f55372d30c
Manobala.S <manovijay383@gmail.com>
2025-03-15 22:06:15 +0530

    Update 127.SIPO_tb.v

--- 7.Counters&Registers/127.SIPO_tb.v
+++ 7.Counters&Registers/127.SIPO_tb.v
@@ -1,1 +1,26 @@
-
+module tb;
+  reg clk,rst,si;
+  wire [3:0] po;
+  sipo sr(.clk(clk),.rst(rst),.si(si),.po(po));
+  initial begin
+    clk = 0;
+    rst = 1;
+    si = 0;
+    #3 rst = 0;
+    si = 1;#3;
+    si = 1;
+    #3 si = 1;
+    #3 si = 1;
+    #100 $finish;
+  end
+  always #2 clk = ~clk;
+  initial begin
+    $monitor("time=%t,po=%0b",$time,po);
+  end
+  initial begin
+    $dumpfile("sipo.vcd");
+    $dumpvars();
+  end
+endmodule
+    
+    
