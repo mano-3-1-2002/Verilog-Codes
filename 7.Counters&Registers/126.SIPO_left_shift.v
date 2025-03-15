716a2fc1b14fba6be243b07b2d02824ca51258ce
Manobala.S <manovijay383@gmail.com>
2025-03-15 22:05:16 +0530

    Update 126.SIPO_left_shift.v

--- 7.Counters&Registers/126.SIPO_left_shift.v
+++ 7.Counters&Registers/126.SIPO_left_shift.v
@@ -1,1 +1,11 @@
 
+module sipo(input clk,rst,si,output reg [3:0] po);
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        po <= 4'b0000;
+      else
+        po <= {po[2:0],si};
+    end
+endmodule
+      
