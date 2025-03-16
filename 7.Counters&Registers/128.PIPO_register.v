e86eb3f8f8593c847ca15172778c29a50fa3e752
Manobala.S <manovijay383@gmail.com>
2025-03-16 21:36:32 +0530

    Update 128.PIPO_register.v

--- 7.Counters&Registers/128.PIPO_register.v
+++ 7.Counters&Registers/128.PIPO_register.v
@@ -1,4 +1,4 @@
-module pipo(input clk,rst,[3:0]pi,output reg [3:0]po);
+odule pipo(input clk,rst,[3:0]pi,output reg [3:0]po);
   always@(posedge clk or posedge rst)begin
     if(rst)
       po <= 4'b0000;
